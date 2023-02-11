76bd5a8b1b0b9b5baff75f52d86db63f0e0d1b5c
Mukambar <45259997+Mukambar@users.noreply.github.com>
2023-02-11 00:41:17 -0800

    5_further_learning_in_sql

---
+++ 5_further_learning_in_sql/what_to_learn_next.sql
@@ -0,0 +1,51 @@
+/*What to learn next
+Google Classroom
+Congratulations on learning SQL!
+Now that you've learned all that, 
+here are some ideas for how to keep going.
+
+Try SQL in other environments
+On Khan Academy, we created a playground for you to 
+try out SQL with small amounts of fake data. We encourage 
+you to keep experimenting with it here, but you'll likely want 
+to use your skills in an environment with real data.
+
+Try SQL for data analysis. 
+These days, SQL is often used to analyze data that comes from all 
+sorts of places. One way that you can start analyzing data immediately
+is with Google's BigQuery, which includes a SQL-like language, and 
+lets you upload data or use public data sets. Learn more from the
+BigQuery documentation.
+
+Try SQL on the server-side. 
+QL can be used to manage the data in an app's server-side database.
+A user uses the app via the "frontend" (webpage or mobile app), 
+the frontend sends HTTP requests to the "backend" (server), and
+the backend issues SQL commands to the database. You can set up 
+a server-side app on your own computer using many different 
+languages/frameworks, two popular stacks are LAMP and LAPP.
+
+Try SQL in a webpage. 
+If you don't have a server setup or data to analyze, you 
+could use SQLite inside a webpage, to store data that the 
+user creates while using the webpage. The data won't be 
+stored when the user leaves the page, however. You can 
+spin-off this example of using SQLite in a webpage.
+
+Learn more SQL
+We covered a lot of SQL, but as you use it more, particularly for 
+creating read/write databases to power apps, you should familiarize 
+yourself with all the features available. You can learn more about 
+indexes and query planning, constraints, triggers, views, and foreign 
+keys. It's best if you know which SQL environment you'll be using those 
+features in first, so you can read the most accurate documentation for it.
+You could also learn different versions of SQL that are used with popular
+database vendors, like MySQL, PostGreSQL, Oracle, MS SQL, and DB2, 
+perhaps in combination with learning SQL on the server-side.
+Go deeper on databases
+SQL is a way to interact with databases, so by learning SQL, you've 
+also learned a fair bit about how databases work. However, you can go 
+much deeper in the area of databases - learning more about the theories, 
+principles, and design - like relational design theory, relational algebra 
+and unified modeling language. One way to learn those is with Stanford's 
+self-paced Intro to Databases class.*/
